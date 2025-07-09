fa6e8a1628aea9bcc0699fd2877838261ea722a4
ekgusev <ekgusev@domclick.ru>
2025-07-09 21:58:51 +0800

    Добавлен сценарий с бесконечным открытием карты

--- app/src/main/java/ru/domclick/mortgage/debug/MapActivity.kt
+++ app/src/main/java/ru/domclick/mortgage/debug/MapActivity.kt
@@ -1,13 +1,17 @@
 package ru.domclick.mortgage.debug
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -18,8 +22,10 @@ import ru.dgis.sdk.DGis
 import ru.dgis.sdk.PersonalDataCollectionConsent
 import ru.dgis.sdk.compose.map.MapComposable
 import ru.dgis.sdk.coordinates.GeoPoint
+import ru.dgis.sdk.getSystemMemoryManager
 import ru.dgis.sdk.map.CameraPosition
 import ru.dgis.sdk.map.Zoom
+import ru.dgis.sdk.platform.HttpOptions
 import ru.dgis.sdk.platform.KeyFromAsset
 import ru.dgis.sdk.platform.KeySource
 import ru.domclick.mortgage.debug.ui.theme.MyApplicationTheme
@@ -37,7 +43,12 @@ class MapActivity : ComponentActivity() {
         val gisContext = DGis.initialize(
             appContext = this,
             keySource = dGisKeySource,
-            dataCollectConsent = PersonalDataCollectionConsent.DENIED
+            dataCollectConsent = PersonalDataCollectionConsent.DENIED,
+            httpOptions = HttpOptions(
+                useCache = true,
+                cacheMaxSize = 10,
+                cacheStoragePath = "/cache"
+            ),
         )
 
         setContent {
@@ -48,7 +59,7 @@ class MapActivity : ComponentActivity() {
                 DisposableEffect(Unit) {
                     onDispose {
                         map?.close()
-                        gisContext.close()
+                        getSystemMemoryManager(gisContext).reduceMemoryUsage()
                     }
                 }
 
@@ -63,10 +74,22 @@ class MapActivity : ComponentActivity() {
                 )
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MapComposable(
-                        modifier = Modifier.padding(innerPadding),
-                        state = state!!,
-                    )
+                    Column {
+                        Button(
+                            modifier = Modifier.padding(innerPadding),
+                            onClick = {
+                                this@MapActivity.startActivity(
+                                    Intent(this@MapActivity, MapActivity::class.java)
+                                )
+                            }) {
+                            Text("Карта")
+                        }
+
+                        MapComposable(
+                            modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                            state = state!!,
+                        )
+                    }
                 }
             }
         }
